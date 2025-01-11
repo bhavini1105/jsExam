a1886f7930d75f523ef767bd3bb2443fadf843f8
bhavini1105 <158131158+bhavini1105@users.noreply.github.com>
2025-01-11 13:23:01 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let bookName = document.getElementById('inputBook');
 let authorName = document.getElementById('inputAuthor');
 let tableData = document.getElementById('table');
-let bookData = [];
 let isEdit = false;
 
-let books = ()=>{
-    let data = JSON.parse(localStorage.getItem("bookData")) || [];
+let books = () => {
+    let data = JSON.parse(localStorage.getItem("book data")) || [];
     return data;
 }
 
@@ -14,81 +13,65 @@ let submitbtn = () => {
 
     if(isEdit){
         let record = {
-            id : id.value,
-            bookName : bookName.value,
-            authorName : authorName.value ,
+            id: Math.floor(Math.random() * 1000),
+            bookName: bookName.value,
+            authorName: authorName.value,
         }
 
         let bookData = books();
-        let oneRecord = bookData.map((r) => (r.id == id) ? record : r);
+        let oneRecord = bookData.map((r) => (r.id == record.id) ? record : r);
 
         localStorage.setItem("book data", JSON.stringify(bookData));
 
         isEdit = false;
-        book_id = " ";
-    }
-    else {
+    } else {
         let newRecord = {
-            id : Math.floor(Math.random() * 1000),
-            bookName : bookName.value,
-            authorName : authorName.value ,
+            id: Math.floor(Math.random() * 1000),
+            bookName: bookName.value,
+            authorName: authorName.value,
         }
 
         let bookData = books();
         bookData.push(newRecord);
-        localStorage.setItem("book data" , JSON.stringify(bookData));
+        localStorage.setItem("book data", JSON.stringify(bookData));
     }
-
 
     bookName.value = '';
     authorName.value = '';
-
+    showData();
 }
 
 let editbtn = (id) => {
-
     let erecord = books();
-
-    let editRecord = erecord.find( (e_id) => {
-        return  e_id.id == id;
-    });
+    let editRecord = erecord.find((e_id) => e_id.id == id);
 
     if(editRecord) {
         isEdit = true;
-        id.value = editRecord.id;
         bookName.value = editRecord.bookName;
         authorName.value = editRecord.authorName;
     }
-
-
-
 }
 
 let deletebtn = (id) => {
-
-    let drecord = books() ;
-
-    let deleteRecord = drecord.filtter((d_id) => {
-        d_id.id != id;
-    });
-
-    localStorage.setItem("book Data" , JSON.stringify(deleteRecord));
+    let drecord = books();
+    let deleteRecord = drecord.filter((d_id) => d_id.id != id);
+    localStorage.setItem("book data", JSON.stringify(deleteRecord));
+    showData();
 }
 
 let showData = () => {
-
     let bookData = books();
 
-    tableData.innerHTML = " ";
+    tableData.innerHTML = "";
 
-    bookData.forEach((book ,index)=>{
-        tableData.innerHTML +=`<tr>
-            <th scope="row">${index+1}</th>
+    bookData.forEach((book, index) => {
+        tableData.innerHTML += `<tr>
+            <th scope="row">${index + 1}</th>
             <td>${book.bookName}</td>
-            <td>${book.bookAuthor}</td>
-            <td><button onclick="return editbtn(${book.id})">Edit</button>  <button onclick="return deletebtn(${book.id})">Delet</button></td>
-          </tr>`
-    })
+            <td>${book.authorName}</td>
+            <td><button onclick="editbtn(${book.id})">Edit</button>  <button onclick="deletebtn(${book.id})">Delete</button></td>
+        </tr>`;
+    });
 }
 
 showData();
